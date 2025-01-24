33ff20b26d67cd93583b40e62224e9593ddc7261
Taylor Southwick <twsouthwick@outlook.com>
2025-01-24 13:18:45 -0800

    add test

--- tools/test-generator/Program.cs
+++ tools/test-generator/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.Xml.Serialization;
 
-var importer = new XmlReflectionImporter();
-var mapping = importer.ImportTypeMapping(typeof(MyClass));
-
 var serializer = new System.Xml.Serialization.XmlSerializer(typeof(MyClass));
+
+var j = new XmlSerializersGenerated.MyClassSerializer();
+
+j.Serialize(Console.Out, new MyClass { Value = 5 });
 
 public class MyClass
 {
     public int Value { get; set; }
 }
+
+
